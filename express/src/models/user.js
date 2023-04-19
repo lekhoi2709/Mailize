672cc0743e142141baae37e349fcd8f504af4aca
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-04-19 20:07:25 +0700

    fix crash server when send duplicate user

--- express/src/models/user.js
+++ express/src/models/user.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-  },
-  phone: {
-    type: String,
-    unique: true,
-  },
-  password: String,
+   username: {
+      type: String,
+      unique: true,
+   },
+   phone: {
+      type: String,
+      unique: true,
+   },
+   password: String,
+   lastName: String,
+   firstName: String,
 });
 
 const User = mongoose.model("User", userSchema);
