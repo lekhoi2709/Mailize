8ed80476cd53847e8fa734fe498831505eaf0e0b
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-04-22 09:01:14 +0700

    render session details

--- mailize/src/pages/inbox/index.tsx
+++ mailize/src/pages/inbox/index.tsx
@@ -1,12 +1,16 @@
 import { signIn, signOut, useSession } from "next-auth/react"
+import { Button } from "@mui/material";
 
 export default function Inbox() {
    const { data: session } = useSession()
    console.log({ session })
 
+
+
    return (
       <div>
-         <p>This is inbox Page</p>
+         <p>Hello, {session?.user?.firstName} {session?.user?.lastName}</p>
+         <Button onClick={() => signOut({ redirect: true, callbackUrl: '/' })}>Log out</Button>
       </div>
    );
 }
