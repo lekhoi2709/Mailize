07067cea4f5611a4b4255771a2bbe0cf9408bd16
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-05-03 13:42:26 +0700

    setup profile page

--- mailize/src/components/dropdown.tsx
+++ mailize/src/components/dropdown.tsx
@@ -3,12 +3,16 @@ import {
    Avatar,
    Grow,
    Box,
-   CardHeader
+   CardHeader,
+   IconButton
 } from "@mui/material"
 
 import LogoutIcon from '@mui/icons-material/Logout';
 
 import { signOut } from 'next-auth/react'
+
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface Props {
    display: boolean,
@@ -19,12 +23,14 @@ interface Props {
 }
 
 export default function Dropdown({ display, lastName, firstName, email, avaLetter }: Props) {
+   const router = useRouter()
+
    return (
       <Grow in={display}>
          <div className="fixed z-40 top-[86px] left-[calc(3%)] w-[94%] rounded-lg bg-[#1b2a2d] md:bg-[#272727] p-2 pt-6 md:left-auto md:right-16 md:top-16 md:w-fit">
             <Box className="bg-[#121212] w-full md:w-fit p-8 rounded-lg ">
                <CardHeader
-                  avatar={<Avatar sx={{ width: 45, height: 45, fontSize: 20 }}>{avaLetter}</Avatar>}
+                  avatar={<IconButton onClick={() => router.push("/mail/profile")}><Avatar sx={{ width: 45, height: 45, fontSize: 20 }}>{avaLetter}</Avatar></IconButton>}
                   title={`${firstName} ${lastName}`}
                   subheader={email}
                   className="pl-0">
