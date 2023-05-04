283a5a5376589b3f4d74490eed450ae0aae79004
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-05-04 22:48:34 +0700

    extend cookie max age

--- express/src/index.js
+++ express/src/index.js
@@ -25,7 +25,7 @@ function App() {
          secret: process.env.SECRET,
          resave: false,
          saveUninitialized: false,
-         cookie: { maxAge: 3600000 }, // 1 hour
+         cookie: { maxAge: 24 * 60 * 60000 }, // 1 day
          store: MongoStore.create({
             mongoUrl: dbUrl,
          }),
