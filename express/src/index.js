a953c171e3c1cd790a45da8bf6046b4ea42e31a6
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-05-07 16:32:51 +0700

    fix db url

--- express/src/index.js
+++ express/src/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const { MONGO_HOST, DB_PORT, DB_NAME } = process.env;
 // var dbUrl = `mongodb://${MONGO_HOST}:${DB_PORT}/${DB_NAME}`;
-var dbUrl = process.env.DB_URL
+var dbUrl = process.env.MONGO_URL
 if (process.env.NODE_ENV == "production") {
    dbUrl = process.env.DB_URL
 }
