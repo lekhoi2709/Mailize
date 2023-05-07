6ecfb541ca9ce1e2f22303552a528f41c3a819be
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-05-07 13:09:38 +0700

    fix mail id undefined

--- mailize/src/pages/mail/[...mailid].tsx
+++ mailize/src/pages/mail/[...mailid].tsx
@@ -85,9 +85,11 @@ export default function MailDetails() {
    const [star, setStar] = useState<boolean>(false)
    const [open, setOpen] = useState<boolean>(false)
    const [restore, setRestore] = useState<boolean>(false)
-   const [data, setData] = useState<string>("{\"root\":{\"children\":[{\"children\":[{\"detail\":0,\"format\":3,\"mode\":\"normal\",\"style\":\"\",\"text\":\"\",\"type\":\"text\",\"version\":1}],\"direction\":\"ltr\",\"format\":\"\",\"indent\":0,\"type\":\"paragraph\",\"version\":1}],\"direction\":\"ltr\",\"format\":\"\",\"indent\":0,\"type\":\"root\",\"version\":1}}")
+   const [loading, setLoading] = useState<boolean>(false)
 
-   const id = mailid![1] ?? ""
+   const timerRef = useRef<ReturnType<typeof setTimeout> | undefined>()
+
+   const id = mailid?.slice(1)
    const path = router.asPath.split('/')
    const time = moment(email?.createAt).format("hh:mm")
    const lastName = email?.from?.name?.split(' ')[0] ?? ""
@@ -140,22 +142,32 @@ export default function MailDetails() {
 
    const fetchApi = useCallback(async () => {
       try {
-         const data = await axios({
-            url: `${process.env.NEXT_PUBLIC_DATABASE_URL}/api/email/id/${id}`,
-            method: "GET",
-            headers: {
-               "Content-Type": "application/json",
-               "Authorization": `Bearer ${session?.user.accessToken}`
-            },
-         })
-         setEmail(data.data.email[0])
+         if (id !== undefined) {
+            const data = await axios({
+               url: `${process.env.NEXT_PUBLIC_DATABASE_URL}/api/email/id/${id}`,
+               method: "GET",
+               headers: {
+                  "Content-Type": "application/json",
+                  "Authorization": `Bearer ${session?.user.accessToken}`
+               },
+            })
+
+            timerRef.current = setTimeout(() => {
+               setEmail(data.data.email[0])
+               setLoading(false)
+            }, 1000)
+         }
       } catch (e) {
          console.log(e)
       }
-   }, [id, session?.user.accessToken])
+   }, [])
+
+   console.log(id)
 
    useEffect(() => {
+      setLoading(true)
       fetchApi()
+      clearTimeout(timerRef.current)
    }, [fetchApi])
 
    useEffect(() => {
@@ -211,63 +223,67 @@ export default function MailDetails() {
             />
          </picture>
 
-         <div className={`w-full h-full backdrop-blur-lg bg-[#121212]/70 overflow-x-hidden overflow-y-hidden`}>
-            <nav className={`flex items-center justify-between mb-4 bg-[#121212] w-full p-2`}>
-               <IconButton onClick={() => router.push(`/mail/${path[2]}`)}>
-                  <ArrowBackIcon />
-               </IconButton>
-               <div className="flex items-center gap-2">
-                  <div className="w-[40px] h-[40px] flex items-center justify-center">
-                     {star ?
-                        <StarIcon className="text-yellow-600"></StarIcon> :
-                        <StarBorderIcon className="text-gray-500"></StarBorderIcon>}
-                  </div>
-                  {email?.trash ?
-                     <IconButton onClick={() => setRestore(true)} >
-                        <RestoreIcon className="text-gray-500"></RestoreIcon>
-                     </IconButton> :
-                     <IconButton onClick={() => setOpen(true)}>
-                        <DeleteOutlineOutlinedIcon className="text-gray-500" />
-                     </IconButton>}
-                  <IconButton>
-                     <MoreVertIcon className="text-gray-500" />
-                  </IconButton>
-               </div>
-            </nav>
-            <div className="md:p-14 md:h-[90%] rounded-lg">
-               <div className="md:bg-[#272727] p-4 md:py-8 md:h-full rounded-lg">
-                  <div className="p-2 px-4 w-full mb-4 md:px-8 md:mb-8">
-                     <h1 className="text-xl break-words">{email?.content.title}</h1>
-                  </div>
-                  <section className="p-2 px-4 w-full flex items-center justify-between mb-4 md:px-12">
-                     <div className="flex gap-4 items-center">
-                        <Avatar sx={{ fontSize: 18 }}>{avaLetter}</Avatar>
-                        <div className="flex flex-col">
-                           <div className="flex gap-2 items-center">
-                              <h1>{email?.from.name}</h1>
-                              <p className="text-sm text-gray-400">{time}</p>
-                           </div>
-                           <p className="text-sm text-gray-500">
-                              to {email?.to.receiver == session?.user.email ? "me" : email?.to.receiver}
-                           </p>
-                        </div>
-                     </div>
-                     <IconButton className="text-gray-500">
-                        <ReplyIcon />
+         <div className={`w-full h-full backdrop-blur-lg bg-[#121212]/70 overflow-x-hidden overflow-y-hidden ${loading ? "flex flex-col items-center justify-center" : ""}`}>
+            {loading ? <CircularProgress /> :
+               <div>
+                  <nav className={`flex items-center justify-between mb-4 bg-[#121212] w-full p-2`}>
+                     <IconButton onClick={() => router.push(`/mail/${path[2]}`)}>
+                        <ArrowBackIcon />
                      </IconButton>
-                  </section>
-                  <section className="p-4 md:px-12">
-                     <LexicalComposer initialConfig={initialConfig}>
-                        <RichTextPlugin
-                           contentEditable={<ContentEditable className='outline-none focus:border focus:border-[#e7e7e7]' />}
-                           placeholder={<div></div>}
-                           ErrorBoundary={LexicalErrorBoundary}
-                        />
-                        <UpdatePlugin />
-                     </LexicalComposer>
-                  </section>
+                     <div className="flex items-center gap-2">
+                        <div className="w-[40px] h-[40px] flex items-center justify-center">
+                           {star ?
+                              <StarIcon className="text-yellow-600"></StarIcon> :
+                              <StarBorderIcon className="text-gray-500"></StarBorderIcon>}
+                        </div>
+                        {email?.trash ?
+                           <IconButton onClick={() => setRestore(true)} >
+                              <RestoreIcon className="text-gray-500"></RestoreIcon>
+                           </IconButton> :
+                           <IconButton onClick={() => setOpen(true)}>
+                              <DeleteOutlineOutlinedIcon className="text-gray-500" />
+                           </IconButton>}
+                        <IconButton>
+                           <MoreVertIcon className="text-gray-500" />
+                        </IconButton>
+                     </div>
+                  </nav>
+                  <div className="md:p-14 md:h-[90%] rounded-lg">
+                     <div className="md:bg-[#272727] p-4 md:py-8 md:h-full rounded-lg">
+                        <div className="p-2 px-4 w-full mb-4 md:px-8 md:mb-8">
+                           <h1 className="text-xl break-words">{email?.content.title}</h1>
+                        </div>
+                        <section className="p-2 px-4 w-full flex items-center justify-between mb-4 md:px-12">
+                           <div className="flex gap-4 items-center">
+                              <Avatar sx={{ fontSize: 18 }}>{avaLetter}</Avatar>
+                              <div className="flex flex-col">
+                                 <div className="flex gap-2 items-center">
+                                    <h1>{email?.from.name}</h1>
+                                    <p className="text-sm text-gray-400">{time}</p>
+                                 </div>
+                                 <p className="text-sm text-gray-500">
+                                    to {email?.to.receiver == session?.user.email ? "me" : email?.to.receiver}
+                                 </p>
+                              </div>
+                           </div>
+                           <IconButton className="text-gray-500">
+                              <ReplyIcon />
+                           </IconButton>
+                        </section>
+                        <section className="p-4 md:px-12">
+                           <LexicalComposer initialConfig={initialConfig}>
+                              <RichTextPlugin
+                                 contentEditable={<ContentEditable className='outline-none focus:border focus:border-[#e7e7e7]' />}
+                                 placeholder={<div></div>}
+                                 ErrorBoundary={LexicalErrorBoundary}
+                              />
+                              <UpdatePlugin />
+                           </LexicalComposer>
+                        </section>
+                     </div>
+                  </div>
                </div>
-            </div>
+            }
          </div>
          <Dialog
             open={open}
